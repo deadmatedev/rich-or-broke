b2c322a30365e5be8763471437307028874cf9cc
Ilya Molchanov <ilia.molchanov@t-systems.com>
2021-09-07 14:45:41 +0300

    make async streaming buffer size configurable

--- src/main/java/com/deadmate/richorbroke/service/impl/AsyncStreamingService.java
+++ src/main/java/com/deadmate/richorbroke/service/impl/AsyncStreamingService.java
@@ -1,21 +1,27 @@
 package com.deadmate.richorbroke.service.impl;
 
+import com.deadmate.richorbroke.config.AsyncStreamingServiceConfig;
 import com.deadmate.richorbroke.service.StreamingService;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.InputStream;
 import java.net.URL;
 
 @Service
+@RequiredArgsConstructor
 public class AsyncStreamingService implements StreamingService {
+
+    private final AsyncStreamingServiceConfig config;
 
     @Override
     public StreamingResponseBody getStreamFrom(final String url) {
         return outputStream -> {
             try (InputStream inputStream = new URL(url).openStream(); outputStream) {
-                StreamUtils.copy(inputStream, outputStream);
+                IOUtils.copyLarge(inputStream, outputStream, new byte [config.getBufferSize()]);
+                outputStream.flush();
             }
         };
     }
--- src/test/java/com/deadmate/richorbroke/service/impl/AsyncStreamingServiceTest.java
+++ src/test/java/com/deadmate/richorbroke/service/impl/AsyncStreamingServiceTest.java
@@ -1,7 +1,11 @@
 package com.deadmate.richorbroke.service.impl;
 
-import com.deadmate.richorbroke.service.StreamingService;
+import com.deadmate.richorbroke.config.AsyncStreamingServiceConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.ByteArrayInputStream;
@@ -14,15 +18,20 @@ import java.net.URLStreamHandler;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class AsyncStreamingServiceTest {
 
-    private final StreamingService service =
-            new AsyncStreamingService();
+    @Mock
+    private AsyncStreamingServiceConfig config;
+    @InjectMocks
+    private AsyncStreamingService service;
 
     @Test
     void givenInMemoryUrlStream_whenStreamingFromUrl_shouldReturnGIFBytes() throws IOException  {
         // given
+        when(config.getBufferSize()).thenReturn(16);
         URL.setURLStreamHandlerFactory(protocol -> "inmemory".equals(protocol)
             ? new URLStreamHandler() {
                 protected URLConnection openConnection(URL url) {
