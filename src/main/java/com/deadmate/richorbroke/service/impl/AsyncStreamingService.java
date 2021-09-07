31c2a038010fe1ddc9083b714a91b9f5fe4bede9
Ilya Molchanov <ilia.molchanov@t-systems.com>
2021-09-07 11:15:04 +0300

    switched to StreamUtils

--- src/main/java/com/deadmate/richorbroke/service/impl/AsyncStreamingService.java
+++ src/main/java/com/deadmate/richorbroke/service/impl/AsyncStreamingService.java
@@ -1,8 +1,8 @@
 package com.deadmate.richorbroke.service.impl;
 
 import com.deadmate.richorbroke.service.StreamingService;
-import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import java.io.InputStream;
@@ -14,9 +14,8 @@ public class AsyncStreamingService implements StreamingService {
     @Override
     public StreamingResponseBody getStreamFrom(final String url) {
         return outputStream -> {
-            try (InputStream inputStream = new URL(url).openStream()) {
-                IOUtils.copyLarge(inputStream, outputStream, new byte[1024 * 64]);
-                outputStream.close();
+            try (InputStream inputStream = new URL(url).openStream(); outputStream) {
+                StreamUtils.copy(inputStream, outputStream);
             }
         };
     }
